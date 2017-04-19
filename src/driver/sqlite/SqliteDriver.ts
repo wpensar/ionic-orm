836462a3bcd523b8aad47986f174517a039bb90b
Marcos Martins <marcos.martins@wpensar.com.br>
2017-04-19 14:34:29 -0300

    corrigindo esquecimento de parametro em funcao

--- src/driver/sqlite/SqliteDriver.ts
+++ src/driver/sqlite/SqliteDriver.ts
@@ -98,7 +98,7 @@ export class SqliteDriver implements Driver {
 
             // we need to enable foreign keys in sqlite to make sure all foreign key related features
             // working properly. this also makes onDelete to work with sqlite.
-            connection.executeSql(`PRAGMA foreign_keys = ON;`, (result: any) => {
+            connection.executeSql(`PRAGMA foreign_keys = ON;`, [], (result: any) => {
                 console.info(result);
                 ok(result);
             }, (error: any) => {
