9d7061ef6f65e0f540ae2794690aaea46e3e5802
Marcos Martins <marcos.martins@wpensar.com.br>
2017-04-19 13:59:10 -0300

    tratando se há plugin de sqlite no projeto

--- src/driver/sqlite/SqliteDriver.ts
+++ src/driver/sqlite/SqliteDriver.ts
@@ -285,8 +285,8 @@ export class SqliteDriver implements Driver {
      * If driver dependency is not given explicitly, then try to load it via "require".
      */
     protected loadDependencies(): void {
-
-        if (!(<any>window).sqlitePlugin.openDatabase) {
+        const sqlite = (<any>window).sqlitePlugin
+        if (!sqlite && !sqlite.openDatabase) {
             throw new DriverPackageNotInstalledError("Sqlite Plugin", "ionic plugin add cordova-sqlite-storage");
         }
     }
